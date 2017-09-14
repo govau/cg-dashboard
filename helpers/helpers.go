08159e45c6344d1151aa50d910d7b037b7916793
Adam Eijdenberg <adam.eijdenberg@digital.gov.au>
2017-09-14 10:38:49 +1000

    Inline utility functions that are only used once

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -1,8 +1,6 @@
 package helpers
 
 import (
-	"crypto/rand"
-	"encoding/base64"
 	"fmt"
 	"net/http"
 	"time"
@@ -52,25 +50,4 @@ func GetValidToken(req *http.Request, settings *Settings) *oauth2.Token {
 
 	// If couldn't find token or if it's expired, return nil
 	return nil
-}
-
-// GenerateRandomBytes returns securely generated random bytes.
-// Borrowed from https://elithrar.github.io/article/generating-secure-random-numbers-crypto-rand/
-func GenerateRandomBytes(n int) ([]byte, error) {
-	b := make([]byte, n)
-	_, err := rand.Read(b)
-	// Note that err == nil only if we read len(b) bytes.
-	if err != nil {
-		return nil, err
-	}
-
-	return b, nil
-}
-
-// GenerateRandomString returns a URL-safe, base64 encoded
-// securely generated random string.
-// Borrowed from https://elithrar.github.io/article/generating-secure-random-numbers-crypto-rand/
-func GenerateRandomString(s int) (string, error) {
-	b, err := GenerateRandomBytes(s)
-	return base64.URLEncoding.EncodeToString(b), err
 }
