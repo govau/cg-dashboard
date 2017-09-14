36c1aa7d330f29105954d77efde7d842d96e6177
Adam Eijdenberg <adam.eijdenberg@digital.gov.au>
2017-09-14 17:11:53 +1000

    More experimental refactoring

--- helpers/settings.go
+++ helpers/settings.go
@@ -1,47 +1,101 @@
 package helpers
 
 import (
+	"crypto/rand"
+	"encoding/base64"
 	"errors"
 	"fmt"
-	"net/url"
 	"os"
 
 	"github.com/cloudfoundry-community/go-cfenv"
 )
 
-// MustGetRedisSettings returns address, password for redis, as determined by looking up:
-// 1. os.Getenv for REDIS_URI
-// 2. VCAP tagged service
-// 3. Default to localhost
-// If bad value, will fatally exit
-func MustGetRedisSettings(env *cfenv.App) (string, string) {
-	// Try to read directly from REDIS_URI first.
-	uri := os.Getenv("REDIS_URI")
-	if uri == "" {
-		// If no direct REDIS_URI, parse VCAP_SERVICES
-		// ignore failure, we'll get an empty string which we default below
-		uri, _ = getRedisService(env)
-	}
-	// If nothing worked so far, default to localhost
-	if uri == "" {
-		uri = "redis://localhost:6379"
-	}
+// EnvLookup will return the value and whether it was found or not
+type EnvLookup func(name string) (string, bool)
 
-	u, err := url.Parse(uri)
-	if err != nil {
-		fmt.Println("unable to parse redis URL, exiting:", err)
-		os.Exit(1)
+// MapEnvLookup creates a lookup based on a map
+func MapEnvLookup(m map[string]string) EnvLookup {
+	return func(name string) (string, bool) {
+		rv, found := m[name]
+		return rv, found
 	}
-
-	password := ""
-	if u.User != nil {
-		password, _ = u.User.Password()
-	}
-
-	return u.Host, password
 }
 
-func getRedisService(env *cfenv.App) (string, error) {
+var (
+	noopLookup = func(string) (string, bool) {
+		return "", false
+	}
+
+	// StdRandStateGenerator will return an appopriate state generator
+	StdRandStateGenerator = func() (string, error) {
+		b := make([]byte, 32)
+		_, err := rand.Read(b)
+		if err != nil {
+			return "", err
+		}
+		return base64.URLEncoding.EncodeToString(b), err
+	}
+)
+
+// MustGet will print a message with the name in it and call os.Exit(1) if value is not set.
+// else it will return value.
+func MustGet(loader EnvLoader, name string) string {
+	rv := loader(name, "")
+	if rv == "" {
+		fmt.Printf("Unable to find '%s' in environment. Exiting.\n", name)
+		os.Exit(1)
+	}
+	return rv
+}
+
+// BoolGet returns true iff the loader is set to the strings "true" or "1"
+func BoolGet(loader EnvLoader, name string) bool {
+	val := loader(name, "false")
+	return val == "true" || val == "1"
+}
+
+// CreateEnvFromCFNamedService looks for a CloudFoundry bound service
+// with the passed name, and will allow sourcing of environment variables
+// from there
+func CreateEnvFromCFNamedService(cfApp *cfenv.App, namedService string) EnvLookup {
+	service, err := cfApp.Services.WithName(namedService)
+	if err != nil {
+		fmt.Printf("Warning: No bound service found with name: %s, will not be used for sourcing env variables\n", namedService)
+		return noopLookup
+	}
+
+	return func(name string) (string, bool) {
+		serviceVar, found := service.Credentials[name]
+		if !found {
+			return "", false
+		}
+		serviceVarAsString, ok := serviceVar.(string)
+		if !ok {
+			fmt.Printf("Warning: variable found in service for %s, but unable to cast as string, so ignoring\n", name)
+			return "", false
+		}
+		return serviceVarAsString, true
+	}
+}
+
+// EnvLoader will return the value or default if not found
+type EnvLoader func(name, defaulVal string) string
+
+// CreateEnvVarLoader will search a path of environment sources until it finds a value
+func CreateEnvVarLoader(path []EnvLookup) EnvLoader {
+	return func(name, defaulVal string) string {
+		for _, env := range path {
+			rv, found := env(name)
+			if found {
+				return rv
+			}
+		}
+		return defaulVal
+	}
+}
+
+// GetRedisService looks for a URI in the cf env
+func GetRedisService(env *cfenv.App) (string, error) {
 	if env == nil {
 		return "", errors.New("Empty Cloud Foundry environment")
 	}
