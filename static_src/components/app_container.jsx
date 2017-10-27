ac780c73a431d0d96f55fb525954683ff31db7d8
Jonathan Ingram <jonathan.ingram@digital.gov.au>
2017-10-27 13:30:40 +1100

    Fix incorrect loading state calculation

--- static_src/components/app_container.jsx
+++ static_src/components/app_container.jsx
@@ -32,7 +32,7 @@ function appReady(app) {
 
 function mapStoreToState() {
   let route;
-  const currentAppGuid = AppStore.currentAppGuid;
+  const { currentAppGuid } = AppStore;
   const app = AppStore.get(currentAppGuid);
   const envRequest = EnvStore.getEnvRequest(currentAppGuid);
   const envUpdateError = EnvStore.getUpdateError(currentAppGuid);
@@ -62,7 +62,11 @@ function mapStoreToState() {
     empty: !AppStore.loading && !appReady(app) && !QuotaStore.loading,
     envRequest,
     envUpdateError,
-    loading: AppStore.loading || QuotaStore.loading,
+    loading:
+      OrgStore.loading ||
+      SpaceStore.loading ||
+      AppStore.loading ||
+      QuotaStore.loading,
     org,
     route,
     quota,
